0be3fd763966a402a7b2d36e6f3a6d0f9859aba4
tanhtanh <73300642+tanhtanh@users.noreply.github.com>
2022-12-02 23:16:32 +0700

    Components
    New Booking component
    
    Co-Authored-By: tanhtanh <73300642+tanhtanh@users.noreply.github.com>

---
+++ src/Pages/Booking/data.js
@@ -0,0 +1,377 @@
+const date = [
+    {
+        month: "12",
+        date: "Thu",
+        day: "1"
+    },
+    {
+        month: "12",
+        date: "Fri",
+        day: "2"
+    },
+    {
+        month: "12",
+        date: "Sat",
+        day: "3"
+    },
+    {
+        month: "12",
+        date: "Sun",
+        day: "4"
+    },
+    {
+        month: "12",
+        date: "Mon",
+        day: "5"
+    },
+    {
+        month: "12",
+        date: "Wed",
+        day: "6"
+    },
+    {
+        month: "12",
+        date: "Tue",
+        day: "7"
+    },
+    {
+        month: "12",
+        date: "Thu",
+        day: "8"
+    },
+
+]
+
+const time = [
+    {
+        showtime: "9:00 PM"
+    },
+    {
+        showtime: "10:00 PM"
+    },
+    {
+        showtime: "11:00 PM"
+    },
+    {
+        showtime: "12:00 PM"
+    },
+    {
+        showtime: "9:00 AM"
+    },
+    {
+        showtime: "10:00 AM"
+    },
+    {
+        showtime: "11:00 AM"
+    },
+    {
+        showtime: "12:00 AM"
+    },
+
+
+]
+
+const form = [
+    {
+        cate: "2D Phụ đề Việt"
+    },
+    {
+        cate: "IMAX2D phụ đề Việt"
+    },
+    {
+        cate: "4D2D phụ đề Việt"
+    }
+]
+
+const theater = [
+    {
+        thea: "CGV Aeonmall Hà Đông"
+    },
+    {
+        thea: "CGV TimesCity"
+    },
+    {
+        thea: "CGV VinCom Bà Triệu"
+    },
+    {
+        thea: "CGV Vincom Times City"
+    }
+
+]
+
+const seat = [
+    {
+        seatId: "1",
+        pass: false
+    },
+    {
+        seatId: "2",
+        pass: false
+    },
+    {
+        seatId: "3",
+        pass: false
+    },
+    {
+        seatId: "4",
+        pass: false
+    },
+    {
+        seatId: "5",
+        pass: false
+    },
+    {
+        seatId: "6",
+        pass: false
+    },
+    {
+        seatId: "7",
+        pass: false
+    },
+    {
+        seatId: "8",
+        pass: false
+    },
+    {
+        seatId: "9",
+        pass: false
+    },
+    {
+        seatId: "10",
+        pass: false
+    },
+    {
+        seatId: "11",
+        pass: false
+    },
+    {
+        seatId: "12",
+        pass: false
+    },
+    {
+        seatId: "13",
+        pass: false
+    },
+    {
+        seatId: "14",
+        pass: false
+    },
+    {
+        seatId: "15",
+        pass: false
+    },
+    {
+        seatId: "16",
+        pass: false
+    },
+    {
+        seatId: "17",
+        pass: false
+    },
+    {
+        seatId: "18",
+        pass: false
+    },
+    {
+        seatId: "19",
+        pass: false
+    },
+    {
+        seatId: "20",
+        pass: false
+    },
+    {
+        seatId: "21",
+        pass: false
+    },
+    {
+        seatId: "22",
+        pass: false
+    },
+    {
+        seatId: "23",
+        pass: false
+    },
+    {
+        seatId: "24",
+        pass: false
+    },
+    {
+        seatId: "25",
+        pass: false
+    },
+    {
+        seatId: "26",
+        pass: false
+    },
+    {
+        seatId: "27",
+        pass: false
+    },
+    {
+        seatId: "28",
+        pass: false
+    },
+    {
+        seatId: "29",
+        pass: false
+    },
+    {
+        seatId: "30",
+        pass: false
+    },
+    {
+        seatId: "31",
+        pass: false
+    },
+    {
+        seatId: "32",
+        pass: false
+    },
+    {
+        seatId: "33",
+        pass: false
+    },
+    {
+        seatId: "34",
+        pass: false
+    },
+    {
+        seatId: "35",
+        pass: false
+    },
+    {
+        seatId: "36",
+        pass: false
+    },
+    {
+        seatId: "37",
+        pass: false
+    },
+    {
+        seatId: "38",
+        pass: false
+    },
+    {
+        seatId: "39",
+        pass: false
+    },
+    {
+        seatId: "40",
+        pass: false
+    },
+    {
+        seatId: "41",
+        pass: false
+    },
+    {
+        seatId: "42",
+        pass: false
+    },
+    {
+        seatId: "43",
+        pass: false
+    },
+    {
+        seatId: "44",
+        pass: false
+    },
+    {
+        seatId: "45",
+        pass: false
+    },
+    {
+        seatId: "46",
+        pass: false
+    },
+    {
+        seatId: "47",
+        pass: false
+    },
+    {
+        seatId: "48",
+        pass: false
+    },
+    {
+        seatId: "49",
+        pass: false
+    },
+    {
+        seatId: "50",
+        pass: false
+    },
+    {
+        seatId: "51",
+        pass: false
+    },
+    {
+        seatId: "52",
+        pass: false
+    },
+    {
+        seatId: "53",
+        pass: false
+    },
+    {
+        seatId: "54",
+        pass: false
+    },
+    {
+        seatId: "55",
+        pass: false
+    },
+    {
+        seatId: "56",
+        pass: false
+    },
+    {
+        seatId: "57",
+        pass: false
+    },
+    {
+        seatId: "58",
+        pass: false
+    },
+    {
+        seatId: "59",
+        pass: false
+    },
+    {
+        seatId: "60",
+        pass: false
+    },
+    {
+        seatId: "61",
+        pass: false
+    },
+    {
+        seatId: "62",
+        pass: false
+    },
+    {
+        seatId: "63",
+        pass: false
+    },
+    {
+        seatId: "64",
+        pass: false
+    },
+    {
+        seatId: "65",
+        pass: false
+    },
+    {
+        seatId: "66",
+        pass: false
+    },
+]
+
+const data = {
+    date,
+    form,
+    time,
+    theater,
+    seat
+}
+
+export default data;
--- src/Pages/Booking/index.js
+++ src/Pages/Booking/index.js
@@ -1,4 +1,92 @@
-const componentName = "Booking";
-export default function () {
-  return <div className={componentName}>{componentName} dddd</div>;
+import React, { useState } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import "./style.css";
+import data from "./data";
+
+
+const DateItem = ({ dateDetail, dateSearch, setDateSearch }) => {
+  return (
+    <button className="date-item-wrapper"
+      style={{
+        border: (dateDetail.day == dateSearch) ? "2px solid rgb(0, 0, 0)" : "2px solid rgb(249, 249, 249)",
+        backgroundColor: "transparent"
+      }}
+      onClick={e => { setDateSearch(dateDetail.day) }}
+    >
+      <div className="left-side">
+        <div className="top">
+          {dateDetail.day}
+
+        </div>
+        <div className="bottom">
+          {dateDetail.date}
+        </div>
+      </div>
+      <div className="right-side">
+        {dateDetail.month}
+      </div>
+    </button>
+
+  )
 }
+
+const CinemaItem = ({ theaDetail }) => {
+
+  const CategoryView = () => {
+
+    const TimeItem = ({ timeDetail }) => {
+      const navigate = useNavigate()
+      return (
+        <button className="time-detail"
+          onClick={e => {
+            navigate("/booking/seat")
+          }}
+        >
+          {timeDetail.showtime}
+        </button>
+      )
+    }
+
+    return (
+      <div className="category-view-wrapper">
+        <div className="time-wrapper list-display">
+          {data.time.map((showtime) => (
+            <TimeItem timeDetail={showtime} />
+          ))}
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <div className="line" />
+      <div className="cinema-detail">
+        <div className="name">{theaDetail.thea}</div>
+        {console.log(theaDetail.thea)}
+        <CategoryView />
+      </div>
+    </div>
+  )
+}
+
+const Booking = () => {
+  const [dateSearch, setDateSearch] = useState(data.date[0].day)
+  return (
+
+    <div className="booking-wrapper">
+      <div className="date-wrapper list-display">
+        {data.date.map((date) => (
+          <DateItem dateDetail={date} dateSearch={dateSearch} setDateSearch={setDateSearch} />
+        ))}
+
+      </div>
+      <div className="cinema-wrapper">
+        {data.theater.map((thea) => (
+          <CinemaItem theaDetail={thea} />
+        ))}
+      </div>
+    </div>
+  )
+}
+export default Booking;
